56a7b9eb43cdca33a8e996cdbb81030e87827f7f
AchrefHamdouni <113561979+AchrefHamdouni@users.noreply.github.com>
2023-05-30 19:48:23 +0100

    11

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-
+import { NativeBaseProvider,Box  } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DoctorLogin from './screens/LoginScreen/DoctorLogin';
+import HomeTest from './screens/LoginScreen/HomeTest';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <NativeBaseProvider>
-    <DoctorLogin />
-  </NativeBaseProvider>
+     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen options={{headerShown:true}} name="DoctorLog" component={DoctorLogin} />
+        <Stack.Screen name="HomeTest" component={HomeTest} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
